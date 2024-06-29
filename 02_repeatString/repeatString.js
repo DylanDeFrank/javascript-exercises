79fe03ad1a8b466405b164a69471261b881dcfe4
Dylan DeFrank <defrankdylan@gmail.com>
2024-06-29 11:07:10 -0700

    Create a function that returns multiple strings
    In the first lesson, learned the beginnings of testing javascript with
    the terminal.
    
    The second lesson required a loop to add the string to an empty
    variable. Went to discord and got help where others had similar issues.
    
    At this time it is helpful to think of parameters as variables I can
    change to return a different result.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    finalResult = ''
+    for (let i=0; i<num; i++) {
+        finalResult += string
+    }   return finalResult
 };
 
 // Do not edit below this line
