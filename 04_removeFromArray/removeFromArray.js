15033bbb0b3e1df7f9946c8b909c12538b5cc432
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-01 21:58:38 -0700

    Create a function that removes items from array
    I did this one very different from what the solution offered. I used the
    for...of to loop through each array. The condition given was that if the
    comparisons match, take the index of that match and splice it out of the
    array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, ...num) {
+        for(const numCheck of num ) {
+          for(const arrayCheck of array) {
+          if (numCheck === arrayCheck) {
+              let index = array.indexOf(numCheck)
+              array.splice(index,1)}
+            }}  for(const numCheck of num ) {
+                  for(const arrayCheck of array) {
+                  if (numCheck === arrayCheck) {
+                    let index = array.indexOf(numCheck)
+                    array.splice(index,1)}
+    } return array}}
 // Do not edit below this line
 module.exports = removeFromArray;
