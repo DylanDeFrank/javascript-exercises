43c1d751eb66655d8ded66154e09eeaa88ca589f
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-03 21:56:28 -0700

    Create a function that returns sum of number range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(start, stop) {
+    let finalSum = 0;
+    if (Number.isInteger(start) == false || Number.isInteger(stop) == false) {
+        return 'ERROR'
+    }
+    else if (String(start).includes('-') == true || String(stop).includes('-') == true) {
+        return 'ERROR'
+    }
+    else if (start > stop) {
+      for(let i = stop; i <= start; i++) {
+        finalSum += i}
+    } 
+    else
+      for(let i = start; i <= stop; i++) {
+        finalSum += i
+    } return finalSum
+  }
 // Do not edit below this line
 module.exports = sumAll;
