b419564c6aef378292bc5804d0df0e3155c14624
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-04 00:16:24 -0700

    Create a function that will convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let x = temp - 32 
+    let y = 5 * x 
+    let finalTemp = y / 9 
+    if (finalTemp == 0)
+      return 0 
+    else 
+      return Number(finalTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let x = temp * 9
+  let y = x / 5 
+  let finalTemp = y + 32 
+  if (finalTemp == 0)
+    return 0 
+  else 
+    return Number(finalTemp.toFixed(1))
 };
 
 // Do not edit below this line
