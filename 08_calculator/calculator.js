28535823b79ff5f9119b21c001bd3bc941b113c0
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-20 22:18:01 -0700

    Create calc functions using javascript

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(...args) {
+  let sum = 0
+  for (const number of args) {
+    sum += number}
+    return sum
+
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+   let ans = a - b
+   return ans
+}; 
+
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0 }
+  else {
+  let sum = array.reduce((sum, current) => sum + current)
+  return sum }
+  }
+;
+
+const multiply = function(array) {
+  let ans = array.reduce((prev, current) => prev * current )
+  return ans
+
+};
+
+const power = function(a, b) {
+  let ans = Math.pow(a, b)
+  return ans
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) {
+    return 1
+  } 
+  let array = []
+  for (i = 1; i <= number; i++) {
+    array.push(i)
+  } let result = array.reduce((a,b) => a * b)
+    return result
 };
 
 // Do not edit below this line
