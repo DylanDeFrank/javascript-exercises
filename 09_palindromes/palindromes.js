53c3fd2cbf9e2188e370ef0e640e8ab38641ff51
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-24 11:32:07 -0700

    Complete the exercises
    Looked at solution for palindrome and completed fibonacci by finding
    the equation and putting it into code. When looking at the solution for
    both I am starting to wonder if I am even understanding js...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    let letters = word.split('')
+    let reverseLetters = letters.slice()
+    reverseLetters.reverse()
+    if (letters.localeCompare(reverseLetters, 'en', {sensitivity: 'base'} == 0) || letters.localeCompare(reverseLetters, 'en', {ignorePunctuation: true}) == 0) {
+      return true 
+    } return false 
+}
+ ;
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    parseInt(number)
+    if (number < 0) {
+      return 'OOPS'
+    } else {
+    return Math.round((Math.pow((1 + Math.sqrt(5)), number) - Math.pow((1 - Math.sqrt(5)), number)) / (Math.pow(2, number) * Math.sqrt(5)))
+    }
 
-};
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
