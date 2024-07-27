6a1c7383778b2d577166c188b33b507d535dfb06
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-27 13:30:18 -0700

    Complete array and object exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    const d = new Date()
+    const indexAge = array.findIndex(array => array.yearOfDeath == undefined)
+    array.map(array => array.age = array.yearOfDeath - array.yearOfBirth)
+      if (indexAge >= 0) {
+        array[indexAge].age = (d.getFullYear() - array[indexAge].yearOfBirth)
+      }
+      array.sort((a, b)=> a.age -b.age)
+      array.reverse()
+      return array[0]  
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
